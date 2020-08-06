bdb9145070d8ea10e1817db79ec3f1b21eb800dc
최성진 <45679609+csj7480@users.noreply.github.com>
2020-08-06 18:04:42 +0900

    start2

---
+++ Isitopen/app/src/main/java/com/example/isitopen/Info.java
@@ -0,0 +1,11 @@
+package com.example.isitopen;
+
+public class Info {
+    public static void main(String[] args){
+        String date;
+        String timeStart;
+        String timeEnd;
+        String loc;
+    }
+    public Info(){}
+}
