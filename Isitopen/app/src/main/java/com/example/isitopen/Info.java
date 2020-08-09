2dc9ac8329159eccee8ec0ea89375ded47351ee0
최성진 <45679609+csj7480@users.noreply.github.com>
2020-08-09 15:27:36 +0900

    class modified

--- Isitopen/app/src/main/java/com/example/isitopen/Info.java
+++ Isitopen/app/src/main/java/com/example/isitopen/Info.java
@@ -1,11 +1,21 @@
 package com.example.isitopen;
 
 public class Info {
-    public static void main(String[] args){
-        String date;
-        String timeStart;
-        String timeEnd;
-        String loc;
+    public String store; // 가게이름
+    public String date; // 검색하려는 날짜
+    public String timeStart; // 오픈시간(검색시간)
+    public String timeEnd; // 마감시간
+    public String loc; // 가게 위치
+
+    public Info(){} // 생성자
+
+    Info(String storeName, String dat, String ts, String te, String locate)
+    {
+        store = storeName;
+        date = dat;
+        timeStart = ts;
+        timeEnd = te;
+        loc = locate;
     }
-    public Info(){}
+
 }
